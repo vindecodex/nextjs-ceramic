b4bec9b43195d669ceda76e8695b4b4fb85ba7fd
vincent villaluna <villalunavincent@gmail.com>
2022-03-09 15:02:18 +0800

    :x: transferred

--- pages/_middleware.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export default async function middleware() {
-				let res = NextResponse.next();
-				return res;
-}
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import { withIronSessionSsr } from 'iron-session/next';
-import { ironSessionOpt } from '../lib/iron-session-opt';
-
-export default function Home() {
-  return (
-					<h1>Home</h1>
-  )
-}
