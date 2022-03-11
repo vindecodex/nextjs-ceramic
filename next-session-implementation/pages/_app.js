0cf107990c31be9180ad0d1ad6b2996b27cd23f3
vincent villaluna <villalunavincent@gmail.com>
2022-03-11 17:33:06 +0800

    wrap app with data provider

--- next-session-implementation/pages/_app.js
+++ next-session-implementation/pages/_app.js
@@ -1,11 +1,12 @@
 import '../styles/globals.css';
-import Header from '../elements/Header';
+import Header from '../elements/vitals/Header';
+import DataProvider from '../elements/provider/DataProvider';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-					<>
+					<DataProvider>
 					<Header />
 									<Component {...pageProps} />
-					</>
+					</DataProvider>
 	)
 }
